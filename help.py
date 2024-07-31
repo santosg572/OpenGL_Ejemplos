69bd7267d98c35a04bd73baa0ecb50e8ddcde4b8
santosg572 <santosg572@gmail.com>
2024-07-31 14:09:08 -0600

    Add files via upload

---
+++ help.py
@@ -0,0 +1,17 @@
+#import OpenGL.GL as gl
+
+#import  OpenGL.GLUT as glut
+
+#import OpenGL.GLU as glu
+
+#print(dir(glu))
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+
+print(help(glBufferData))
+
+
+
